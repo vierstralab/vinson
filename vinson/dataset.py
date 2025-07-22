b5b51cc544ad01975d1fa778fbedf0d929f1e735
jvierstra <jvierstra@altius.org>
2025-07-22 10:08:10 -0700

    Modified the one-hot encoder to allow for degenerate IUPAC characters

--- vinson/dataset.py
+++ vinson/dataset.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
-import numba
-
+]
 import h5py
 
 import torch
@@ -10,103 +9,7 @@ from torch.utils.data import Dataset
 from genome_tools import GenomicInterval
 from genome_tools.data.extractors import FastaExtractor
 
-@numba.njit("void(int8[:, :], int8[:], int8[:])", cache=True)
-def _fast_one_hot_encode(X_ohe, seq, mapping):
-    """An internal function for quickly converting bytes to one-hot indexes."""
-
-    for i in range(len(seq)):
-        idx = mapping[seq[i]]
-        if idx == -1:
-            continue
-
-        if idx == -2:
-            raise ValueError(
-                "Encountered character that is not in " + "`alphabet` or in `ignore`."
-            )
-
-        X_ohe[i, idx] = 1
-
-
-def one_hot_encode(
-    sequence,
-    alphabet=["A", "C", "G", "T"],
-    dtype=float,
-    ignore=["N"],
-    desc=None,
-    verbose=False,
-    **kwargs,
-):
-    """Converts a string or list of characters into a one-hot encoding.
-
-    This function will take in either a string or a list and convert it into a
-    one-hot encoding. If the input is a string, each character is assumed to be
-    a different symbol, e.g. 'ACGT' is assumed to be a sequence of four
-    characters. If the input is a list, the elements can be any size.
-
-    Although this function will be used here primarily to convert nucleotide
-    sequences into one-hot encoding with an alphabet of size 4, in principle
-    this function can be used for any types of sequences.
-
-    Parameters
-    ----------
-    sequence : str or list
-            The sequence to convert to a one-hot encoding.
-
-    alphabet : set or tuple or list
-            A pre-defined alphabet where the ordering of the symbols is the same
-            as the index into the returned tensor, i.e., for the alphabet ['A', 'B']
-            the returned tensor will have a 1 at index 0 if the character was 'A'.
-            Characters outside the alphabet are ignored and none of the indexes are
-            set to 1. Default is ['A', 'C', 'G', 'T'].
-
-    dtype : str or torch.dtype, optional
-            The data type of the returned encoding. Default is int8.
-
-    ignore: list, optional
-            A list of characters to ignore in the sequence, meaning that no bits
-            are set to 1 in the returned one-hot encoding. Put another way, the
-            sum across characters is equal to 1 for all positions except those
-            where the original sequence is in this list. Default is ['N'].
-
-
-    Returns
-    -------
-    ohe : numpy.ndarray
-            A binary matrix of shape (alphabet_size, sequence_length) where
-            alphabet_size is the number of unique elements in the sequence and
-            sequence_length is the length of the input sequence.
-    """
-
-    for char in ignore:
-        if char in alphabet:
-            raise ValueError(
-                "Character {} in the alphabet ".format(char)
-                + "and also in the list of ignored characters."
-            )
-
-    if isinstance(alphabet, list):
-        alphabet = "".join(alphabet)
-
-    ignore = "".join(ignore)
-
-    e = "utf8"
-    seq_idxs = np.frombuffer(bytearray(sequence, e), dtype=np.int8)
-    alpha_idxs = np.frombuffer(bytearray(alphabet, e), dtype=np.int8)
-    ignore_idxs = np.frombuffer(bytearray(ignore, e), dtype=np.int8)
-
-    one_hot_mapping = np.zeros(256, dtype=np.int8) - 2
-    for i, idx in enumerate(alpha_idxs):
-        one_hot_mapping[idx] = i
-
-    for i, idx in enumerate(ignore_idxs):
-        one_hot_mapping[idx] = -1
-
-    n, m = len(sequence), len(alphabet)
-
-    one_hot_encoding = np.zeros((n, m), dtype=np.int8)
-    _fast_one_hot_encode(one_hot_encoding, seq_idxs, one_hot_mapping)
-    return one_hot_encoding.astype(dtype).T
-
+from .utils import one_hot_encode
 
 class SequenceEmbeddingDataset(Dataset):
     def __init__(
---
+++ vinson/utils.py
@@ -0,0 +1,96 @@
+import numpy as np
+
+import numba
+
+
+IUPAC_DNA = "XACMGRSVTWYHKDBN"
+
+_dna_embed = np.zeros((256, 4), dtype=np.int8)
+_dna_embed[ord("A")] = np.array([1, 0, 0, 0])
+_dna_embed[ord("C")] = np.array([0, 1, 0, 0])
+_dna_embed[ord("G")] = np.array([0, 0, 1, 0])
+_dna_embed[ord("T")] = np.array([0, 0, 0, 1])
+_dna_embed[ord("W")] = np.array([1, 0, 0, 1])
+_dna_embed[ord("S")] = np.array([0, 1, 1, 0])
+_dna_embed[ord("M")] = np.array([1, 1, 0, 0])
+_dna_embed[ord("K")] = np.array([0, 0, 1, 1])
+_dna_embed[ord("R")] = np.array([1, 0, 1, 0])
+_dna_embed[ord("Y")] = np.array([0, 1, 0, 1])
+_dna_embed[ord("B")] = np.array([0, 1, 1, 1])
+_dna_embed[ord("D")] = np.array([1, 0, 1, 1])
+_dna_embed[ord("H")] = np.array([1, 1, 0, 1])
+_dna_embed[ord("V")] = np.array([1, 1, 1, 0])
+_dna_embed[ord("N")] = np.array([1, 1, 1, 1])
+
+
+@numba.njit("void(int8[:, :], int8[:], int8[:, :])", cache=True)
+def _fast_one_hot_encode(X_ohe, seq, mapping):
+    """Fast encoding of characters with a reference embedding
+
+    Parameters
+    ----------
+    X_ohe : numpy.ndarray
+        The one-hot encoding matrix of shape (sequence_length, 4),
+        of type int8
+    seq : numpy.ndarray
+        Array of utf-8 encoded characters of type int8
+    mapping : numpy.ndarray
+        Array of mapping where indicies are utf-8 encoded characters
+        of shape (256, 4) where the the first the indicies correspond
+        to the UTF-8 encoding of a character, of type int8
+    """
+    for i in range(len(seq)):
+        base_embed = mapping[seq[i]]
+        X_ohe[i, :] = base_embed
+
+
+def one_hot_encode(sequence, dtype=np.float32):
+    """_Converts a string or list of characters into a one-hot encoding.
+
+    Parameters
+    ----------
+    sequence : str or list
+        The sequence to convert to a one-hot encoding.
+    dtype : str or torch.dtype, optional
+        _description_, by default np.float32
+
+    Returns
+    -------
+    ohe: numpy.ndarray
+        A matrix of shape (4, sequence_length)
+    """
+    seq_idxs = np.frombuffer(bytearray(sequence, "utf8"), dtype=np.int8)
+    n, m = len(sequence), _dna_embed.shape[1]
+    one_hot_encoding = np.zeros((n, m), dtype=np.int8)
+    _fast_one_hot_encode(one_hot_encoding, seq_idxs, _dna_embed)
+
+    one_hot_encoding = (
+        one_hot_encoding.astype(dtype) / one_hot_encoding.sum(axis=1)[:, np.newaxis]
+    )
+    return one_hot_encoding.T
+
+
+def get_iupac_char_from_alleles(alleles):
+    """Returns the IUPAC character from a list of possible
+    DNA nucleotides.
+
+    Parameters
+    ----------
+    alleles : list
+        A list of DNA characters
+
+    Returns
+    -------
+    iupac : str
+        The list of alleles encoded in a IUPAC DNA
+        base identity
+    """
+
+    i = None
+    for allele in alleles:
+        j = IUPAC_DNA.find(allele)
+        if i is None:
+            i = j
+        else:
+            i = i | j
+    return IUPAC_DNA[i]
