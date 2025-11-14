c7b3e3fbbd427972a34ebfd60f39ee752240080d
wishabc <aswq22013@gmail.com>
2025-11-14 11:58:43 +0300

    generate data script

---
+++ nextflow/bin/generate_validation_data.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pandas as pd
+import argparse
+import anndata as ad
+
+from genome_tools.data.anndata import read_zarr_backed
+from genome_tools.data.extractors import TabixExtractor
+from genome_tools import df_to_genomic_intervals
+
+from vinson.utils.data_formatting import data_to_h5, extract_data_from_backed_anndata, sanitize_data
+
+
+def get_bg_for_peaks(peaks_df, stats_path):
+    intervals = df_to_genomic_intervals(peaks_df)
+    rows = []
+    with TabixExtractor(stats_path) as extractor:
+        for interval in intervals:
+            df_slice = extractor[interval].query('fit_type == "segment')
+            assert len(df_slice) == 1, "Expected exactly one matching stats row per peak"
+            rows.append(df_slice)
+    stats = pd.concat(rows)
+    bg = stats.eval('bg_r * bg_p / (1 - bg_p)').values
+    return bg
+
+
+def generate_data_from_sample_peaks(anndata: ad.AnnData, sample_ids) -> dict:
+    anndata_slice = anndata[sample_ids, :]
+    data = []
+    for sample_id, row in anndata_slice.obs.iterrows():
+        sample_slice = anndata_slice[sample_id, :]
+        fit_stats_file = row['hotspot3_fit_stats_file']
+        peaks = pd.read_table(row['peaks_file_0.01fdr']).drop(columns=['start']).rename({'summit': 'start'})
+        
+        peaks['end'] = peaks['start'] + 1
+        data_bundle = {
+            'dhs_id': f'{sample_id}.' + peaks.index.astype(str).values,
+            'sample_id': np.full(len(peaks), sample_id, dtype=np.str_),
+            'read_depth': np.full(len(peaks), row['nuclear_reads'], dtype=np.float32),
+            'chrom': peaks['#chr'].values,
+            'summit': peaks['summit'].values,
+            'background': get_bg_for_peaks(peaks, fit_stats_file),
+            'class': np.ones(len(peaks), dtype=np.int8),
+            'density': peaks['summit_density'].values,
+            'indiv_id': np.full(len(peaks), sample_slice.obsm['indiv_id'], dtype=np.str_),
+        }
+        data.append(data_bundle)
+    
+    data = {k: np.concatenate([d[k] for d in data]) for k in data[0].keys()}
+
+    return sanitize_data(data)
+
+def check_none(val):
+    if val is None or val == 'None' or pd.isna(val):
+        return False
+    return True
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Generate validation data")
+    parser.add_argument("anndata_file", type=str, help="Input AnnData file.")
+    parser.add_argument("output_file", type=str, help="Output HDF5 file for validation data.")
+    parser.add_argument("--mode", type=str, choices=['sample_peaks', 'sample_dhs', 'all_dhs'], default='all_dhs', help="Mode of validation data generation.")
+    parser.add_argument("--sample_ids", type=str, nargs='*', default=[], help="List of sample IDs to include in subset mode.")
+    parser.add_argument("--dhs_ids", type=str, nargs='*', default=[], help="List of DHS IDs to include in subset mode.")
+    args = parser.parse_args()
+
+    anndata = read_zarr_backed(args.anndata_file)
+    sample_ids = [sid for sid in args.sample_ids if check_none(sid)]
+    dhs_ids = [did for did in args.dhs_ids if check_none(did)]
+
+    if len(dhs_ids) == 0:
+        assert len(sample_ids) > 0, "Either sample IDs or DHS IDs must be provided."
+        if args.mode == 'sample_peaks':
+            print('Generating validation data for sample peaks')
+            # Logic to generate validation data for sample peaks
+            data = generate_data_from_sample_peaks(anndata, sample_ids=sample_ids)
+        else:
+            use_sample_peaks = args.mode == 'sample_dhs'
+            print('Generating validation data for sample DHSs')
+            data = extract_data_from_backed_anndata(
+                anndata,
+                sample_ids=sample_ids,
+                use_sample_peaks=use_sample_peaks
+            )
+    else:
+        print('DHS IDs are provided. Ignoring "--mode" argument')
+        data = extract_data_from_backed_anndata(
+            anndata,
+            sample_ids=sample_ids,
+            dhs_ids=dhs_ids
+        )
+    data_to_h5(args.output_file, data)
+
+    
--- nextflow/generate_data.nf
+++ nextflow/generate_data.nf
@@ -1,20 +1,62 @@
 
-process generate_cell_selective_data {
+process generate_sample_validation_data {
 
     conda "${params.conda}"
-    publishDir "${params.outdir}/"
+    publishDir "${params.outdir}/validation_data"
+
+    input:
+        tuple val(sample_id), val(mode)
 
     output:
-        path "*.${suffix}"
+        tuple val(prefix), path(name)
 
     script:
-    suffix = "cell_selective_data.h5"
+    prefix = "${sample_id}.${mode}"
+    name = "${prefix}.validation_data.h5"
     """
-    python3 $moduleDir/bin/generate_cell_selective_data.py \
-        --adata ${params.zarr_anndata} \
-        --suffix ${suffix}
+    python3 $moduleDir/bin/generate_validation_data.py \
+        ${params.zarr_anndata} \
+        ${name} \
+        --sample_ids ${sample_id} \
+        --mode ${mode}
     """
 }
 
+
+process generate_dhs_validation_data {
+
+    conda "${params.conda}"
+    publishDir "${params.outdir}/validation_data"
+
+    input:
+        val dhs_id
+
+    output:
+        tuple val(prefix), path(name)
+
+    script:
+    prefix = "${dhs_id}"
+    name = "${prefix}.validation_data.h5"
+    """
+    python3 $moduleDir/bin/generate_validation_data.py \
+        ${params.zarr_anndata} \
+        ${name} \
+        --dhs_ids ${dhs_id}
+    """
+}
+
+workflow {
+    Channel.fromPath(params.validation_samples_file)
+        | splitCsv(header:true, sep:'\t')
+        | map(row -> tuple(row.sample_id, row.mode))
+        | generate_sample_validation_data
+}
+
+workflow dhsValidation {
+    Channel.fromPath(params.validation_dhs_file)
+        | splitCsv(header:true, sep:'\t')
+        | map(row -> row.dhs_id)
+        | generate_dhs_validation_data
+}
 
 
