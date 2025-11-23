265c7c9841fa2aab31761c7517ddbcfb197914e9
wishabc <aswq22013@gmail.com>
2025-11-23 21:30:11 +0300

    fix

--- nextflow/bin/generate_validation_data.py
+++ nextflow/bin/generate_validation_data.py
@@ -13,8 +13,8 @@ def get_bg_for_peaks(peaks_df: pd.DataFrame, stats_path):
     stats = pd.read_table(stats_path).query('fit_type == "segment"')
     merged = peaks_df[['#chr', 'summit']].merge(stats, on="#chr", how="left")
     merged['has_bg'] = merged.eval('summit >= start & summit < end')
-    if merged['has_bg'].sum() != len(peaks_df):
-        print(merged.query('~has_bg'), flush=True)
+    tmp = merged.groupby(['#chr', 'summit'])['has_bg'].max()
+    print(tmp[tmp == 0])
     bg = merged.query('has_bg').eval('bg_r * bg_p / (1 - bg_p)').values
     assert len(bg) == len(peaks_df), f"Background length mismatch {len(bg)} vs {len(peaks_df)}"
     return bg
