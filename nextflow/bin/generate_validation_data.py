6a423ad44a104ad30a610639ff209f4cd9280e1c
wishabc <aswq22013@gmail.com>
2025-11-23 21:10:54 +0300

    fix

--- nextflow/bin/generate_validation_data.py
+++ nextflow/bin/generate_validation_data.py
@@ -10,10 +10,11 @@ from vinson.utils.data_formatting import data_to_h5, sanitize_data
 
 
 def get_bg_for_peaks(peaks_df: pd.DataFrame, stats_path):
-    stats = pd.read_table(stats_path)
+    stats = pd.read_table(stats_path).query('fit_type == "segment"')
     merged = peaks_df[['#chr', 'summit']].merge(stats, on="#chr", how="left")
     merged = merged.query('summit >= start & summit < end')
     bg = merged.eval('bg_r * bg_p / (1 - bg_p)').values
+    assert len(bg) == len(peaks_df)
     return bg
 
 
