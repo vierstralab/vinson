5fc389773df7f0e7748faffd85aa81046e1e6bfc
wishabc <aswq22013@gmail.com>
2026-01-27 17:56:21 +0700

    fix

--- src/vinson/utils/data_formatting/adata_utils.py
+++ src/vinson/utils/data_formatting/adata_utils.py
@@ -29,7 +29,11 @@ def update_layers_dict(layers: dict, train_adata: ad.AnnData, suffix: str):
 
 
 def get_number_of_train_examples(anndata_file):
-    adata = ad.read_h5ad(anndata_file)
+    full_adata = ad.read_h5ad(anndata_file)
+    adata = full_adata[
+        full_adata.obsm["split_data"] == "train",
+        full_adata.varm["split_data"] == "train",
+    ]
     if 'n_training_examples' in adata.uns:
         n_examples = adata.uns['n_training_examples']
     else:
