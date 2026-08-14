58e43a843ca7d55c9a5d14ab3245571cc23c41f9
Sergey Abramov <aswq22013@gmail.com>
2026-08-15 02:37:16 +0700

    fix

--- src/vinson/utils/data_formatting/readers.py
+++ src/vinson/utils/data_formatting/readers.py
@@ -55,9 +55,9 @@ def parse_genotype_file(genotype_file):
 def _parse_indiv_info(adata_slice, data):
     if "genotype_id" in adata_slice.obs:
         data["indiv_id"] = adata_slice.obs["genotype_id"]
-    if "genotype_cluster" in adata_slice.obs:
+    elif "genotype_cluster" in adata_slice.obs:
         # Deprecated format
-        data["indiv_id"] = adata_slice.obs["genotype_cluster"].fillna('')
+        data["indiv_id"] = adata_slice.obs["genotype_cluster"]
     elif "indiv_id" in adata_slice.obsm:
         # Deprecated format
         print('Deprecated: indiv_id found in .obsm; expected .obs["genotype_cluster"]')
